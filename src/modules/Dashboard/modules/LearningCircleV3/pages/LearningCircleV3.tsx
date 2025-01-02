8d00f5c353a541f75981dae89ccbfccd925bf7de
abijith-suresh <abijithsuresh47@gmail.com>
2025-01-02 21:38:45 +0530

    style(eventcard): Updated the styles of event card

--- src/modules/Dashboard/modules/LearningCircleV3/pages/LearningCircleV3.tsx
+++ src/modules/Dashboard/modules/LearningCircleV3/pages/LearningCircleV3.tsx
@@ -7,9 +7,9 @@ const LearningCircleV3: React.FC = () => {
       <EventCard
         image="https://via.placeholder.com/400x200"
         title="Digital Marketing Workshop"
-        subtitle="College of Engineering Trivandrum"
-        date="January 15, 2025"
-        time="10:00 AM - 12:00 PM"
+        subtitle="College of Engineering Trivandrum Sreekaryam, Trivandrum"
+        date="Jan 15"
+        time="10:00 AM"
         location="CET Main Hall"
         joinedText="5 people you might know have joined"
       />
