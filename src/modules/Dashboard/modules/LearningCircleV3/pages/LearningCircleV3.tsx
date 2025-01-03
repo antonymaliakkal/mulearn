f8768b4da1a7d1cfdc8799028567d2bb25308d14
Amith M P <amithmp676@gmail.com>
2025-01-04 01:30:16 +0530

    ui changes for learning circle

--- src/modules/Dashboard/modules/LearningCircleV3/pages/LearningCircleV3.tsx
+++ src/modules/Dashboard/modules/LearningCircleV3/pages/LearningCircleV3.tsx
@@ -3,30 +3,38 @@ import AllLearningCircles from "../components/AllLearningCircles/AllLearningCirc
 import LearningCircleWithUpcomingMeets from "../components/LearningCircleWithUpcomingMeets/LearningCircleWithUpcomingMeets";
 import styles from "./LearningCircleV3.module.css";
 
+// Static image import
+import Event1 from "../../../assets/images/Events/Event1.png";
+import Event2 from "../../../assets/images/Events/Event2.png";
+import Event3 from "../../../assets/images/Events/Event3.png";
+import Event4 from "../../../assets/images/Events/Event4.png";
+
 const LearningCircleV3: React.FC = () => {
-    const upcomingEvents = Array.from({ length: 4 }).map((_, index) => ({
-        image: "https://via.placeholder.com/400x200",
-        title: `Event ${index + 1}`,
-        subtitle: "College of Engineering Trivandrum",
-        date: `Jan ${15 + index}`,
-        time: "10:00 AM",
-        location: "CET Main Hall",
-        joinedText: `${5 + index} people you might know have joined`
-    }));
+  const eventImages = [Event1, Event2, Event3, Event4];
 
-    const allEvents = Array.from({ length: 4 }).map((_, index) => ({
-        image: "https://via.placeholder.com/400x200",
-        title: `All Event ${index + 1}`,
-        subtitle: "Another Venue",
-        joinedText: `${2 + index} people you might know have joined`
-    }));
+  const upcomingEvents = eventImages.map((image, index) => ({
+    image,
+    title: `Event ${index + 1}`,
+    subtitle: "College of Engineering Trivandrum",
+    date: `Jan ${15 + index}`,
+    time: "10:00 AM",
+    location: "CET Main Hall",
+    joinedText: `${5 + index} people you might know have joined`,
+  }));
 
-    return (
-        <div className={styles.mainBody}>
-            <LearningCircleWithUpcomingMeets events={upcomingEvents} />
-            <AllLearningCircles circles={allEvents} />
-        </div>
-    );
+  const allEvents = eventImages.map((image, index) => ({
+    image,
+    title: `All Event ${index + 1}`,
+    subtitle: "College of Engineering Trivandrum",
+    joinedText: `${2 + index} people you might know have joined`,
+  }));
+
+  return (
+    <div className={styles.mainBody}>
+      <LearningCircleWithUpcomingMeets events={upcomingEvents} />
+      <AllLearningCircles circles={allEvents} />
+    </div>
+  );
 };
 
 export default LearningCircleV3;
