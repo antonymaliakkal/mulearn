8c2411d1c3505a04a5897bdcf246a26e9eecf1c0
unnikrishnang628 <unnikrishnang628@gmail.com>
2025-01-03 11:41:37 +0000

    completed opportunities section

--- src/modules/Dashboard/modules/Opportunity/pages/Opportunity.tsx
+++ src/modules/Dashboard/modules/Opportunity/pages/Opportunity.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Opportunity: React.FC<Props> = () => {
 
-  const [opportunities,setOpportunities] = useState([
+  const opportunities = [
     {
       title : "Content Writer",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -57,7 +57,7 @@ const Opportunity: React.FC<Props> = () => {
       jobType: "Part-time",
       link: "#"
     }
-  ])
+  ];
 
   return (
     <div className={styles.mainDiv}>
