80cdd8b295de547885d1d7f055f4c632e976f7f9
Sharun Raj Nambayil <sharunraj39@gmail.com>
2025-01-04 23:19:13 +0530

    InterestGroup done

--- src/modules/DashboardV2/modules/InterestGroupV2/service/InterestGroupService.tsx
+++ src/modules/DashboardV2/modules/InterestGroupV2/service/InterestGroupService.tsx
@@ -1,2 +1,13 @@
 import { reqInstance } from "../../../services/baseReqInstance";
 import { InterestGroupRoutes } from "../../../services/dasboardEndpoints";
+
+export const getInterestGroup = async () => {
+    try {
+        const response = await reqInstance.get(InterestGroupRoutes.listAllInterestGroups);
+        console.log(response.data); // Log the actual data
+        return response.data; // Return the data for further use
+    } catch (error) {
+        console.error("Error fetching interest groups:", error);
+        throw error; // Re-throw the error for upstream handling
+    }
+}
